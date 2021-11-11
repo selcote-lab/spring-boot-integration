1100effe9a082538cf3bb51a56602c78988b47b6
zouhaire <zouhaire@idside.de>
2021-11-11 17:43:38 +0100

    init repo

---
+++ src/main/java/com/tonasolution/springbootintegration/SpringBootIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.tonasolution.springbootintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tonasolution/springbootintegration/SpringBootIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonasolution.springbootintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
